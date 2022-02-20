4397d8a440604d945ff31a0f26bde4496f1fbd6f
Josh <josh@joshlang.ca>
2022-02-20 14:36:55 -0800

    Package updates

--- Epoche.MVVM.Presentation.WinUI/EpochePageBase.cs
+++ Epoche.MVVM.Presentation.WinUI/EpochePageBase.cs
@@ -6,6 +6,9 @@ namespace Epoche.MVVM.Presentation.WinUI;
 
 public abstract class EpochePageBase<TViewModel> : Page, ITarget<TViewModel> where TViewModel : NavigableViewModelBase
 {
-    public IViewModelContainer<TViewModel> ViewModelContainer { get; } = EpocheServiceProviderBaseSetup.ServiceProvider?.GetRequiredService<IViewModelContainer<TViewModel>>() ?? throw new InvalidOperationException($"Use {nameof(IEpocheServiceProviderSetup)}.{nameof(IEpocheServiceProviderSetup.SetServiceProvider)} before using {nameof(EpochePageBase<NavigableViewModelBase>)}");
+    public IViewModelContainer<TViewModel> ViewModelContainer { get; } = 
+        EpocheServiceProviderBaseSetup.ServiceProvider?.GetRequiredService<IViewModelContainer<TViewModel>>() ??
+        throw new InvalidOperationException($"Use {nameof(IEpocheServiceProviderSetup)}.{nameof(IEpocheServiceProviderSetup.SetServiceProvider)} before using {nameof(EpochePageBase<NavigableViewModelBase>)}");
+
     public ChildComponentContainer ChildComponentContainer { get; } = new();
 }
--- Epoche.MVVM.Presentation.WinUI/EpocheUserControlBase.cs
+++ Epoche.MVVM.Presentation.WinUI/EpocheUserControlBase.cs
@@ -5,5 +5,7 @@ using Microsoft.UI.Xaml.Controls;
 namespace Epoche.MVVM.Presentation.WinUI;
 public abstract class EpocheUserControlBase<TViewModel> : UserControl where TViewModel : ViewModelBase
 {
-    public TViewModel ViewModel { get; } = EpocheServiceProviderBaseSetup.ServiceProvider?.GetRequiredService<TViewModel>() ?? throw new InvalidOperationException($"Use {nameof(IEpocheServiceProviderSetup)}.{nameof(IEpocheServiceProviderSetup.SetServiceProvider)} before using {nameof(EpocheUserControlBase<ViewModelBase>)}");
+    public TViewModel ViewModel { get; } =
+        EpocheServiceProviderBaseSetup.ServiceProvider?.GetRequiredService<TViewModel>() 
+        ?? throw new InvalidOperationException($"Use {nameof(IEpocheServiceProviderSetup)}.{nameof(IEpocheServiceProviderSetup.SetServiceProvider)} before using {nameof(EpocheUserControlBase<ViewModelBase>)}");
 }
